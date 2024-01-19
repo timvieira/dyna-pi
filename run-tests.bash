0ae71f207d628cb8488ea32596f41738b3d98c77
Tim Vieira <tim.f.vieira@gmail.com>
2024-01-19 17:03:10 -0500

    ()

--- run-tests.bash
+++ run-tests.bash
@@ -1,26 +1,3 @@
-#!/usr/bin/env bash
-function red    { echo -e "\e[31m$@\e[0m"; }
-function yellow { echo -e "\e[33m$@\e[0m"; }
-function green  { echo -e "\e[32m$@\e[0m"; }
-function blue   { echo -e "\e[34m$@\e[0m"; }
-function purple { echo -e "\e[35m$@\e[0m"; }
-function cyan   { echo -e "\e[36m$@\e[0m"; }
-
-function bright_red    { echo -e "\e[31;1m$@\e[0m"; }
-function bright_yellow { echo -e "\e[33;1m$@\e[0m"; }
-function bright_green  { echo -e "\e[32;1m$@\e[0m"; }
-function bright_blue   { echo -e "\e[34;1m$@\e[0m"; }
-function bright_purple { echo -e "\e[35;1m$@\e[0m"; }
-function bright_cyan   { echo -e "\e[36;1m$@\e[0m"; }
-
-
-function run {
-    echo
-    green "****************************************************************************"
-    green "$@"
-    python $@
-}
-
 COVERAGE="$@"
 
 if [ -z "$COVERAGE" ]; then
