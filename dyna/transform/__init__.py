a954d860569907b113470cb402ee21591e4f0a34
Tim Vieira <tim.f.vieira@gmail.com>
2026-06-18 23:24:05 -0400

    Export deforestation transform

--- dyna/transform/__init__.py
+++ dyna/transform/__init__.py
@@ -2,3 +2,8 @@ from dyna.transform.fold import Fold, fold_r_S, prune_r_S
 from dyna.transform.unfold import Unfold
 from dyna.transform.slash import Slash
 from dyna.transform.lct import LCT
+from dyna.transform.deforest import (
+    DeforestDifferenceLists,
+    DifferenceListDeforestationSpec,
+    deforest_difference_lists,
+)
